a865a7015df2c9f60e27acf761b8f4e92c5d35b0
Magnus Sølver Simonsen <mav.solver@hotmail.com>
2026-04-16 13:24:02 +0200

    Class work in progress

---
+++ src/main/java/app/bigOExamples/entitiy/CompareClass.java
@@ -0,0 +1,31 @@
+/*
+package app.bigOExamples.entitiy;
+import java.util.Objects;
+public class CompareClass implements Comparable<CompareClass>  {
+
+    private int value;
+
+    public CompareClass(int value){
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CompareClass c = (CompareClass) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+
+}
+
+ */
