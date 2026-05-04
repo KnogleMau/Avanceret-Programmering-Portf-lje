76552a4be4bec1b2d6eb62dec093a111bf2cb503
Magnus Sølver Simonsen <mav.solver@hotmail.com>
2026-05-04 12:41:08 +0200

    Tilføjet tree Searech med Searchstrategies

---
+++ src/main/java/app/treesearch/SearchStrategies.java
@@ -0,0 +1,87 @@
+package app.treesearch;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class SearchStrategies {
+
+    public static void main(String[] args) {
+
+        Node root = new Node(10); // root
+        Node newChild = root.addChild(5); // root first child
+        Node newChildChild = newChild.addChild(115); // root first child -> child
+        newChildChild.addChild(207); // root first child -> child -> child
+        newChildChild.addChild(500);
+        root.addChild(7); // root second child
+        newChild = root.addChild(15); // root third child
+        newChild.addChild(117);
+
+        Node foundNode = searchBFS(117, root); // BFS search
+        System.out.println("BFS starting..");
+        if (foundNode != null) {
+            System.out.println(foundNode.getValue());
+        } else {
+            System.out.println("Found nothing!");
+        }
+
+        foundNode = searchDFS(117, root); // BFS search
+        System.out.println("DFS starting..");
+        if (foundNode != null) {
+            System.out.println(foundNode.getValue());
+        } else {
+            System.out.println("Found nothing!");
+        }
+    }
+
+    public static Node searchBFS(int value, Node root) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            System.out.print("Current queue: ");
+            for (Node item : queue) {
+                System.out.print(item.getValue() + " ");
+            }
+            System.out.println();
+
+            Node currentNode = queue.remove();
+
+            if (currentNode.getValue() == value) {
+                System.out.println("Finished searching!");
+                return currentNode;
+            } else {
+                queue.addAll(currentNode.getChildren());
+            }
+        }
+        return null;
+    }
+
+    public static Node searchDFS(int value, Node root) {
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            System.out.print("Current stack: ");
+            for (Node item : stack) {
+                System.out.print(item.getValue() + " ");
+            }
+            System.out.println();
+
+            Node currentNode = stack.pop();
+
+            if (currentNode.getValue() == value) {
+                System.out.println("Finished searching!");
+                return currentNode;
+            } else {
+                for (Node child : currentNode.getChildren()) {
+                    stack.push(child);
+                }
+            }
+        }
+        return null;
+    }
+
+
+
+}
